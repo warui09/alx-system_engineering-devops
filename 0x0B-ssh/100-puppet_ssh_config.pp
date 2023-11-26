ea14d8548b25e35ee273f657852293994c6d83eb
Kevin Warui <waruimacharia@gmail.com>
2023-11-26 12:18:54 -0800

    Edit 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # use Puppet to make changes to ssh configuration file
 
-file {'etc/ssh/ssh_config':
+file {'/etc/ssh/ssh_config':
   ensure  => file,
 }
 
 file_line {'IdentityFile':
-  path      => 'etc/ssh/ssh_config',
+  path      => '/etc/ssh/ssh_config',
   file_line => 'IdentityFile ~/.ssh/school',
   match     => '^IdentityFile',
 }
 
 file_line {'NoPassword':
-  path      => 'etc/ssh/ssh_config',
+  path      => '/etc/ssh/ssh_config',
   file_line => 'PasswordAuthentication no',
   match     => '^PasswordAuthentication',
 }
