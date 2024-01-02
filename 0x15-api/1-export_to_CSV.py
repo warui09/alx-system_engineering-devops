f6841fef6038ee2b19b39e1a4a5d2bd2b6b70ce4
Kevin Warui <waruimacharia@gmail.com>
2024-01-02 09:50:05 -0800

    Add 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,4 +4,26 @@ Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
 """
 
+import csv
+import requests
+import sys
 
+if sys.argv[1]:
+    user_id = sys.argv[1]
+
+url = "https://jsonplaceholder.typicode.com/"
+
+name = requests.get(f"{url}users/{user_id}").json()["name"]
+user_name = name.split(" ")[0]
+
+url_tasks = f"{url}users/{user_id}/todos"
+
+all_tasks = requests.get(url_tasks).json()
+task_list = []
+
+
+with open(f"{user_id}.csv", "w") as csv_file:
+    my_writer = csv.writer(csv_file, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for task in all_tasks:
+        my_writer.writerow([user_id, user_name, task["completed"],
+            task["title"]])
