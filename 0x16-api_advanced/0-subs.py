ec2ca5c0bb96b406098301fc30dda743e604a203
Kevin Warui <waruimacharia@gmail.com>
2024-01-16 05:48:30 -0800

    Add 2-recurse.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,9 @@ import requests
 def number_of_subscribers(subreddit):
     """gets the number of subscribers of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-agent": "Mozilla/5.0"}
     try:
-        response = requests.get(url, headers={"User-agent": "Mozilla/5.0"})
+        response = requests.get(url, headers, allow_redirects=False)
         subscribers = response["data"]["subscribers"]
         return subscribers
     except requests.RequestException as e:
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns a list containing the titles of all hot articles
+for a given subreddit"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    headers = {"User-agent": "Mozilla/5.0"}
+
+    params = {}
+    if after:
+        params["after"] = after
+
+    try:
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+        )
+        response.raise_for_status()
+
+        data = response.json()
+
+        if "data" in data and "children" in data["data"]:
+            posts = data["data"]["children"]
+            for post in posts:
+                hot_list.append(post["data"]["title"])
+
+        if data.get("data", {}).get("after"):
+            recurse(subreddit, hot_list, after=data["data"]["after"])
+    except requests.RequestException as e:
+        if (
+            isinstance(e, requests.exceptions.HTTPError)
+            and e.response.status_code == 302
+        ):
+            print(f"Invalid subreddit: {subreddit}")
+            return None
+        else:
+            print(f"Error fetching data: {e}")
+            return None
+
+    return hot_list
