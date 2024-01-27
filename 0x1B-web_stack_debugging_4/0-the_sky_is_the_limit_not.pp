2f9ed7c82f870bbad232f8a8f406fbcbe2abbc37
Kevin Warui <waruimacharia@gmail.com>
2024-01-27 05:11:42 -0800

    Edit 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,29 +1,8 @@
 # increase the maximum number of files nginx can start
 
-class { 'nginx_open_file_limits':
-  file_max_limit     => 70000,
-  nofile_soft_limit  => 10000,
-  nofile_hard_limit  => 30000,
-}
-
-class nginx_open_file_limits (
-  Int $file_max_limit,
-  Int $nofile_soft_limit,
-  Int $nofile_hard_limit,
-) {
-  file { '/etc/sysctl.conf':
-    ensure  => file,
-    content => "fs.file-max = ${file_max_limit}\n",
-    notify  => Exec['sysctl-reload'],
-  }
-
-  file { '/etc/security/limits.conf':
-    ensure  => file,
-    content => "nginx soft nofile ${nofile_soft_limit}\nnginx hard nofile ${nofile_hard_limit}\n",
-  }
-
-  exec { 'sysctl-reload':
-    command => 'sysctl -p',
-    refreshonly => true,
-  }
+exec { 'adjust_nginx_file_limit':
+  command     => 'sed -i "5s/[0-9]\+/$(`ulimit -n`)/" /etc/default/nginx && service nginx restart',
+  refreshonly => true,
+  onlyif      => 'test -e /etc/default/nginx',
+  provider    => shell,
 }
